5bd728389c1c10ef645c94b488825414b5910f08
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2017-05-24 18:11:01 -0300

    Adding implementation for stem and creating the result string from TokenStream.

--- src/main/java/br/ufrgs/inf/cmp269/luceneTester/AnalisysPerformer.java
+++ src/main/java/br/ufrgs/inf/cmp269/luceneTester/AnalisysPerformer.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.HashSet;
-import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.analysis.es.SpanishLightStemFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.AttributeFactory;
@@ -18,7 +19,19 @@ import org.apache.lucene.util.AttributeFactory;
  */
 public class AnalisysPerformer {
     
-    private String target;
+    public static void main(String args[]){
+        AnalisysPerformer a = new AnalisysPerformer(new AnalysisOption[]{
+            AnalysisOption.STEM, 
+            AnalysisOption.STOP_WORDS
+        });
+        a.analyze("Estados Unidos instó hoy a Corea\n" +
+" del Norte, en una reunión bilateral en la ONU, a acelerar los\n" +
+" trámites para que empiecen cuanto antes las inspecciones\n" +
+" internacionales de sus instalaciones nucleares declaradas, aceptadas\n" +
+" en esta jornada finalmente por Pyongyang.");
+    }
+    
+    private String originalTarget;
     private HashSet<AnalysisOption> options;
     
     public AnalisysPerformer(AnalysisOption[] options) {
@@ -26,53 +39,49 @@ public class AnalisysPerformer {
     }
     
     public String analyze(String target){
-        this.target = target;
+        this.originalTarget = target;
         
-        if(options.contains(AnalysisOption.STOP_WORDS)){
-            removeStopWords();
-        }
-        
-        if(options.contains(AnalysisOption.SYNONYMS)){
-            
-        }
-        
-        //Synonym
-        
-        //Stem
-        
-        //Analyze
-        
-        //
-        
-        return this.target;
-    }
-    
-    /**
-     * Removes stop words from the query.
-     */
-    private void removeStopWords() {
-        CharArraySet stopWords = SpanishAnalyzer.getDefaultStopSet();
         AttributeFactory factory = AttributeFactory.DEFAULT_ATTRIBUTE_FACTORY;
         StandardTokenizer standardTokenizer = new StandardTokenizer(factory);
-        standardTokenizer.setReader(new StringReader(target));
+        standardTokenizer.setReader(new StringReader(originalTarget));
         TokenStream tokenStream = standardTokenizer;
-        tokenStream = new StopFilter(tokenStream, stopWords);
-
+        
+        System.out.println("ANTES LOWER");
+        System.out.println(getAnalyzedTarget(tokenStream));
+        tokenStream = new LowerCaseFilter(tokenStream);
+         
+        System.out.println("ANTES STOP");
+        System.out.println(getAnalyzedTarget(tokenStream));
+        if(options.contains(AnalysisOption.STOP_WORDS)){
+            tokenStream = new StopFilter(tokenStream, SpanishAnalyzer.getDefaultStopSet());
+        }
+        System.out.println("ANTES STEM");
+        System.out.println(getAnalyzedTarget(tokenStream));
+        
+        
+        if(options.contains(AnalysisOption.STEM)){
+            tokenStream = new SpanishLightStemFilter(tokenStream);
+        }
+        
+        return getAnalyzedTarget(tokenStream);
+    }
+    
+    public String getAnalyzedTarget(TokenStream tokenStream){
+        String target;
         CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
         try {
             StringBuilder sb = new StringBuilder();
+            
             tokenStream.reset();
             while (tokenStream.incrementToken()) {
                 String term = charTermAttribute.toString();
-                sb.append(term + " ");
+                sb.append(term).append(" ");
             }
             target = sb.toString();
         } catch (IOException exception) {
-            System.out.println("[ERROR] Unable to remove stopwords. Error: " + exception.getMessage());
+            target = this.originalTarget;
+            System.out.println("[ERROR] Unable to stringfy token stream. Error: " + exception.getMessage());
         }
-    }
-    
-    private void addSynonym(){
-        
+        return target;
     }
 }
